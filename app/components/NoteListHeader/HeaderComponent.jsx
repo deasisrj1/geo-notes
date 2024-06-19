11ff1ad72b4c54ca14e6c0a898438389f5cd4881
deasisrj1 <rdeasis@ualberta.ca>
2024-06-19 03:20:36 -0700

    FIN: view public notes when logged in

--- app/components/NoteListHeader/HeaderComponent.jsx
+++ app/components/NoteListHeader/HeaderComponent.jsx
@@ -3,14 +3,14 @@ import { NEW_NOTE, MY_NOTES, PUBLIC_NOTES } from "@/app/enums/noteEnums";
 export default function HeaderComponent({ currentTab, setCurrentTab, user }) {
   return (
     <nav className="flex flex-row p-4 bg-neutral-950 ">
-      <div className="container flex items-center justify-between ">
+      <div className="container flex items-center justify-between overflow-x-auto ">
         <ul className="flex space-x-2">
           {user ? (
             <>
               <li>
                 <button
                   onClick={() => setCurrentTab(NEW_NOTE)}
-                  className={`p-4 w-full text-white hover:text-gray-300 rounded-lg ${
+                  className={`text-nowrap p-4 w-full text-white hover:text-gray-300 rounded-lg ${
                     currentTab == NEW_NOTE ? "bg-neutral-800" : ""
                   }`}
                 >
@@ -20,16 +20,37 @@ export default function HeaderComponent({ currentTab, setCurrentTab, user }) {
               <li>
                 <button
                   onClick={() => setCurrentTab(MY_NOTES)}
-                  className={`p-4 w-full text-white hover:text-gray-300 rounded-lg ${
+                  className={`text-nowrap p-4 w-full text-white hover:text-gray-300 rounded-lg ${
                     currentTab == MY_NOTES ? "bg-neutral-800" : ""
                   }`}
                 >
                   My Notes
                 </button>
               </li>
+              <li>
+                <button
+                  onClick={() => setCurrentTab(PUBLIC_NOTES)}
+                  className={`text-nowrap p-4 w-full text-white hover:text-gray-300 rounded-lg ${
+                    currentTab == PUBLIC_NOTES ? "bg-neutral-800" : ""
+                  }`}
+                >
+                  Public Notes
+                </button>
+              </li>
             </>
           ) : (
-            <></>
+            <>
+              <li>
+                <button
+                  onClick={() => setCurrentTab(PUBLIC_NOTES)}
+                  className={`text-nowrap p-4 w-full text-white hover:text-gray-300 rounded-lg ${
+                    currentTab == PUBLIC_NOTES ? "bg-neutral-800" : ""
+                  }`}
+                >
+                  Public Notes
+                </button>
+              </li>
+            </>
           )}
         </ul>
       </div>
