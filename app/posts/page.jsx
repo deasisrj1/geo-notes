f0a2ee4f96ce790427049b834894fd47858d252f
whatwhatgoose <aryathedoggo@gmail.com>
2024-06-24 01:26:05 -0700

    tbd posts with reply WIP demo

---
+++ app/posts/page.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import Comment from "../components/Replies/Comment";
+import Assignment from "../components/Replies/Assignment";
+import Tags from "../components/Replies/Tags";
+import Header from "../components/Posts/Header";
+import ReplyForm from "../components/Posts/ReplyForm";
+const activity = [
+  {
+    id: 1,
+    type: "comment",
+    person: { name: "Eduardo Benz", href: "#" },
+    imageUrl:
+      "https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. ",
+    date: "6d ago",
+  },
+  {
+    id: 2,
+    type: "assignment",
+    person: { name: "Hilary Mahy", href: "#" },
+    assigned: { name: "Kristin Watson", href: "#" },
+    date: "2d ago",
+  },
+  {
+    id: 3,
+    type: "tags",
+    person: { name: "Hilary Mahy", href: "#" },
+    tags: [
+      { name: "Bug", href: "#", color: "fill-red-500" },
+      { name: "Accessibility", href: "#", color: "fill-indigo-500" },
+    ],
+    date: "6h ago",
+  },
+  {
+    id: 4,
+    type: "comment",
+    person: { name: "Jason Meyers", href: "#" },
+    imageUrl:
+      "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.",
+    date: "2h ago",
+  },
+];
+
+export default function Posts() {
+  return (
+    <div className="flow-root">
+      <Header />
+      <br />
+      <ul role="list" className="-mb-8 px-8">
+        {activity.map((activityItem, activityItemIdx) => (
+          <li key={activityItem.id}>
+            <div className="relative pb-8">
+              {activityItemIdx !== activity.length - 1 ? (
+                <span
+                  className="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200"
+                  aria-hidden="true"
+                />
+              ) : null}
+              <div className="relative flex items-start space-x-3">
+                {activityItem.type === "comment" ? (
+                  <Comment activityItem={activityItem} />
+                ) : activityItem.type === "assignment" ? (
+                  <Assignment activityItem={activityItem} />
+                ) : activityItem.type === "tags" ? (
+                  <Tags activityItem={activityItem} />
+                ) : null}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+    <div className="px-8 pb-8">
+        <ReplyForm />
+    </div>
+      
+    </div>
+  );
+}
